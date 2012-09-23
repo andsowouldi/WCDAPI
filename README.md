78d91053f2340af9369dbf75e6e53b339823689d
Eric Trizis <trizis@gmail.com>
2012-09-23 11:43:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+WCDAPI
+---------------
+This is a basic PowerShell v3 script to connect to and use the WCD API.
+The only requirements are that you have PowerShell v3 installed and that you enter your password where it says [Your Password].
+The only thing it will do by default is print a list of the first page of threads in the Library forum.
---
+++ wcd.ps1
@@ -0,0 +1,39 @@
+﻿Add-Type -Assembly System.ServiceModel.Web,System.Runtime.Serialization
+
+$url = "https://what.cd/login.php"
+
+$CookieContainer = New-Object System.Net.CookieContainer
+
+$postData = "username=andsowouldi&password=[Your Password]"
+
+$buffer = [text.encoding]::ascii.getbytes($postData)
+
+[net.httpWebRequest] $req = [net.webRequest]::create($url)
+$req.method = "POST"
+$req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+$req.Headers.Add("Accept-Language: en-US")
+$req.Headers.Add("Accept-Encoding: gzip,deflate")
+$req.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+$req.AllowAutoRedirect = $false
+$req.ContentType = "application/x-www-form-urlencoded"
+$req.ContentLength = $buffer.length
+$req.TimeOut = 50000
+$req.KeepAlive = $true
+$req.Headers.Add("Keep-Alive: 300");
+$req.CookieContainer = $CookieContainer
+$reqst = $req.getRequestStream()
+$reqst.write($buffer, 0, $buffer.length)
+$reqst.flush()
+$reqst.close()
+[net.httpWebResponse] $res = $req.getResponse()
+$resst = $res.getResponseStream()
+$sr = new-object IO.StreamReader($resst)
+$result = $sr.ReadToEnd()
+
+$web = new-object net.webclient
+$web.Headers.add("Cookie", $res.Headers["Set-Cookie"])
+$forum = $web.DownloadString("https://what.cd/ajax.php?action=forum&type=viewforum&forumid=26") | ConvertFrom-JSON
+$forum.response.threads | ForEach-Object {
+   $_.title
+}
+$res.close()
